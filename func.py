192812c709753c8154b9475b8e68a7922e0b60a9
Diptendu <77498066+diptendu38@users.noreply.github.com>
2023-12-18 11:31:02 +0530

    key fetch

--- func.py
+++ func.py
@@ -22,7 +22,7 @@ def handler(ctx, data: dict = None):
     )'''
 
 
-import io
+'''import io
 import json
 
 from fdk import response
@@ -45,9 +45,41 @@ def handler(ctx, data: io.BytesIO=None):
 
     print("Vale of name = ", name, flush=True)
     print("Exiting Python Hello World handler", flush=True)
-       
+
     return response.Response(
         ctx, response_data=json.dumps(result),
         headers={"Content-Type": "application/json"},
-    )
+    )'''
+
+import oci
+import base64
+
+def fetch_secret_from_vault(secret_ocid):
+    # Set up OCI Vault client
+    vault_client = oci.secrets.SecretsClient({})
+
+    # Fetch the secret content
+    secret_content = vault_client.get_secret_bundle(secret_ocid).data.secret_bundle_content.content
+
+    return secret_content
+
+def handle(ctx, data: bytes = None):
+    # Replace 'your_secret_ocid' with the actual OCID of your secret in OCI Vault
+    secret_ocid = 'ocid1.vaultsecret.oc1.ap-mumbai-1.amaaaaaampahd3ianjpywinnvcdy5se5kidb25wsheuw5j5ddjznqvuvy6qq'
+
+    try:
+        # Fetch the secret content from OCI Vault
+        secret_content = fetch_secret_from_vault(secret_ocid)
+
+        # Decode the base64-encoded content (assuming the secret is a base64-encoded PEM key)
+        decoded_content = base64.b64decode(secret_content).decode('utf-8')
+
+        # Print the secret content
+        print(f"Secret Content:\n{decoded_content}")
+
+        return f"Secret Content:\n{decoded_content}"
+    except oci.exceptions.ServiceError as e:
+        print(f"Error fetching secret from OCI Vault: {e}")
+        return f"Error fetching secret from OCI Vault: {e}"
+
 
