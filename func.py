fff71dac63f252e4312f0b6171e4001fc4fd5089
Diptendu <77498066+diptendu38@users.noreply.github.com>
2023-12-18 18:34:10 +0530

    nkkn

--- func.py
+++ func.py
@@ -54,8 +54,8 @@ def handler(ctx, data: io.BytesIO=None):
         cfg = dict(ctx.Config())
         client_private_key_ocid = cfg["client_private_key_ocid"]
         logging.getLogger().info("Client Private Key OCID = " + client_private_key_ocid)
-        client_private_key_ocid = cfg["client_public_key_ocid"]
-        logging.getLogger().info("Client Public Key OCID = " + client_public_key_ocid)
+        #client_private_key_ocid = cfg["client_public_key_ocid"]
+        #logging.getLogger().info("Client Public Key OCID = " + client_public_key_ocid)
         server_public_key_ocid = cfg["server_public_key_ocid"]
         logging.getLogger().info("Server Public Key OCID = " + server_public_key_ocid)
     except Exception as e:
