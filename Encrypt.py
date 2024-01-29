6c155e2b27828fe5cde2675d71fa1e229e710c73
Diptendu <77498066+diptendu38@users.noreply.github.com>
2024-01-29 16:53:06 +0530

    Update Encrypt.py

--- Encrypt.py
+++ Encrypt.py
@@ -2,6 +2,8 @@ import string
 import secrets
 from Crypto.Cipher import AES
 import base64
+from Crypto.Util.Padding import pad
+
 
 class RequestSignatureEncryptedValue:
     IVECTOR_LENGTH = 16
@@ -16,29 +18,33 @@ class RequestSignatureEncryptedValue:
         random_key = ''.join(secrets.choice(characters) for _ in range(length))
         return random_key
 
+
     def encrypt(self, data, key, ivector):
-        try:
-            cipher = AES.new(key, AES.MODE_CBC, iv=ivector)
-            data_bytes = data.encode('utf-8')
-            padded_data = data_bytes + b'\0' * (16 - len(data_bytes) % 16)
-            iv_and_data = ivector + padded_data
-            encrypted_value = cipher.encrypt(iv_and_data)
-            return encrypted_value
-        except Exception as exp:
-            print(f"Exception occurred during encryption: {exp}")
-            return b''  
+            try:
+                data_with_iv = ivector + data.encode('utf-8')
+                cipher = AES.new(key, AES.MODE_CBC, ivector)
+                padded_data = pad(data_with_iv, AES.block_size)
+                encrypted_data = cipher.encrypt(padded_data)
+                return encrypted_data
+            except Exception as exp:
+                print(f"Exception occurred: {exp}")
+                return None
+
 
     def encode(self, value):
         if value is not None:
-            return base64.b64encode(value)
+            return base64.b64encode(value).decode('utf-8')
         else:
             print("Encryption failed. Cannot encode None value.")
-            return b''
+            return ''
+
 
     def generate_request_signature_encrypted_value(self, jwt_token):
         symmetric_key_value = self.generate_random(32)
         ivector = self.generate_random_ivector()
         aes_encryption_data = self.encrypt(jwt_token, symmetric_key_value.encode(), ivector)
-        request_signature_encrypted_value = self.encode(aes_encryption_data).decode('utf-8')
-        return request_signature_encrypted_value, symmetric_key_value
 
+        print(f"AES Symmetric Key: {symmetric_key_value}")
+        request_signature_encrypted_value = self.encode(aes_encryption_data)
+        print(f"Request Signature Encrypted Value: {request_signature_encrypted_value}")
+        return request_signature_encrypted_value, symmetric_key_value
