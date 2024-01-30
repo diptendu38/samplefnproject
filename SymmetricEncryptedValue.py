ad61c8de8a58c1f63e1db028ce0fb5d119a10d0d
Diptendu <77498066+diptendu38@users.noreply.github.com>
2024-01-30 11:39:10 +0530

    Update SymmetricEncryptedValue.py

--- SymmetricEncryptedValue.py
+++ SymmetricEncryptedValue.py
@@ -1,57 +1,8 @@
-'''from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives import serialization, hashes
-from cryptography.hazmat.primitives.asymmetric import padding
-import base64
-import oci
-
-def read_key_from_vault(key_ocid):
-    signer = oci.auth.signers.get_resource_principals_signer()
-    try:
-        client = oci.secrets.SecretsClient({}, signer=signer)
-        key_content = client.get_secret_bundle(key_ocid).data.secret_bundle_content.content.encode('utf-8')
-        key_bytes = base64.b64decode(key_content)
-    except Exception as ex:
-        print("ERROR: failed to retrieve the key from the vault", ex)
-        raise
-    return key_bytes
-
-def encrypt_with_public_key(data, public_key):
-    cipher_text = public_key.encrypt(
-        data,
-        padding.OAEP(
-            mgf=padding.MGF1(algorithm=hashes.SHA256()),
-            algorithm=hashes.SHA256(),
-            label=None
-        )
-    )
-    return cipher_text
-
-def symmetrickeyEncryption(data, public_key_ocid):
-    public_key_bytes = read_key_from_vault(public_key_ocid)
-    data_bytes = data.encode('utf-8')
-
-    bank_public_key = serialization.load_pem_public_key(
-        public_key_bytes,
-        backend=default_backend()
-    )
-
-    if bank_public_key is not None:
-        encrypted_data = encrypt_with_public_key(data_bytes, bank_public_key)
-
-        if encrypted_data is not None:
-            encrypted_base64 = base64.b64encode(encrypted_data).decode('utf-8')
-            return encrypted_base64
-        else:
-            print("Encryption failed.")
-            return ''
-    else:
-        print("Public key loading failed.")
-        return ''
-'''
-
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import serialization, hashes
 from cryptography.hazmat.primitives.asymmetric import padding
+from Crypto.Cipher import PKCS1_v1_5
+from Crypto.PublicKey import RSA
 import base64
 import oci
 
@@ -67,15 +18,21 @@ def read_key_from_vault(key_ocid):
     return key_bytes
 
 def encrypt_with_public_key(data, public_key):
-    cipher_text = public_key.encrypt(
-        data,
-        padding.OAEP(
-            mgf=padding.MGF1(algorithm=hashes.SHA256()),
-            algorithm=hashes.SHA256(),
-            label=None
-        )
-    )
-    return cipher_text #recent
+        cipher = PKCS1_v1_5.new(public_key)
+        if isinstance(data, str):
+            message_bytes = data.encode('utf-8')
+        elif isinstance(data, bytes):
+            message_bytes = data
+        else:
+            raise ValueError("Unsupported data type. Please provide a string or bytes.")
+        chunk_size = 245
+        chunks = [message_bytes[i:i + chunk_size] for i in range(0, len(message_bytes), chunk_size)]
+
+        encrypted_data = b""
+        for chunk in chunks:
+            encrypted_chunk = cipher.encrypt(chunk)
+            encrypted_data += encrypted_chunk
+        return encrypted_data
 
 def symmetrickeyEncryption(data, public_key_ocid):
     public_key_bytes = read_key_from_vault(public_key_ocid)
