dd9aff799e3d2a2e1d15ed72486d9c2aee5d02b3
Diptendu <77498066+diptendu38@users.noreply.github.com>
2024-01-30 13:38:48 +0530

    Update SymmetricDecrypt.py

--- SymmetricDecrypt.py
+++ SymmetricDecrypt.py
@@ -1,44 +1,55 @@
 from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives import serialization, hashes
-from cryptography.hazmat.primitives.asymmetric import padding
-from Crypto.PublicKey import RSA
-from Crypto.Cipher import PKCS1_v1_5
+from cryptography.hazmat.primitives import serialization
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import unpad
 import base64
+import jwt
 import oci
 
-def read_key_from_vault(key_ocid):
-    signer = oci.auth.signers.get_resource_principals_signer()
-    try:
-        client = oci.secrets.SecretsClient({}, signer=signer)
-        cert_content = client.get_secret_bundle(key_ocid).data.secret_bundle_content.content
-        cert_bytes = base64.b64decode(cert_content)
-        public_key = RSA.import_key(cert_bytes)
-        return public_key
-    except Exception as ex:
-        print("ERROR: failed to retrieve the certificate from the vault - {}".format(ex))
-        raise
+class Decryptor:
+    
+    def decrypt(encrypted_value, key):
+        try:
+            ivector = encrypted_data[:16]
+            cipher = AES.new(key, AES.MODE_CBC, ivector)
+            decrypted_data = cipher.decrypt(encrypted_data[16:]) 
+            unpadded_data = unpad(decrypted_data, AES.block_size)
+            return unpadded_data.decode('utf-8')
+        except ValueError as ve:
+            print(f"Decryption failed: {ve}")
+            return None
+        except Exception as exp:
+            print(f"Exception occurred during AES decryption: {exp}")
+            return None
 
-def decrypt_with_private_key(encrypted_data, private_key):
-    try:
-        cipher = PKCS1_v1_5.new(private_key)
-        encrypted_bytes = base64.b64decode(encrypted_data)
-        decrypted_data = cipher.decrypt(encrypted_bytes, None)
-        return decrypted_data.decode('utf-8')
-        
-    except Exception as e:
-        print(f"Decryption failed: {str(e)}")
-        return None
+    def read_key_from_vault(key_ocid):
+        signer = oci.auth.signers.get_resource_principals_signer()
+        try:
+            client = oci.secrets.SecretsClient({}, signer=signer)
+            key_content = client.get_secret_bundle(key_ocid).data.secret_bundle_content.content.encode('utf-8')
+            key_bytes = base64.b64decode(key_content)
+        except Exception as ex:
+            print("ERROR: failed to retrieve the key from the vault", ex)
+            raise
+        return key_bytes
 
-def key_decryption_logic(encrypted_base64, private_key_ocid):
-    private_key = read_key_from_vault(private_key_ocid)
+    def generate_response_signature_decrypted_value(self,symmetric_key_value,request_signature_encrypted_value,public_key_ocid):
+        decrypted_jws_token_bytes = Decryptor.decrypt(base64.b64decode(request_signature_encrypted_value), symmetric_key_value.decode('utf-8'))
 
-    if private_key is not None:
-        decrypted_data = decrypt_with_private_key(encrypted_base64,private_key)
-
-        if decrypted_data is not None:
-            return decrypted_data
+        if decrypted_jws_token_bytes  is not None:
+            print(f"Decrypted JWS Token: {decrypted_jws_token_bytes}")
         else:
             print("Decryption failed.")
-            return b""
-    else:
-        print("Private key loading failed.")
+
+        public_key_bytes = Decryptor.read_key_from_vault(public_key_ocid)
+        public_key = serialization.load_pem_public_key(
+        public_key_bytes,
+        backend=default_backend()
+    )
+        try:
+            decoded_payload = jwt.decode(decrypted_jws_token, public_key, algorithms=['RS256'])
+            return(decoded_payload)
+        except jwt.ExpiredSignatureError:
+            print("JWT has expired.")
+        except jwt.InvalidTokenError as e:
+            print(f"Invalid token: {e}")
